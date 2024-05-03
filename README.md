67ea1cd21a0026ed44d5e83695570bd3e1ebe76f
SopheaEVC <sopheaff@123gmail.com>
2024-05-03 17:19:36 +0700

    new added

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Rong-ARG
 This file aims to create Koh Rong Archipelegos. 
-The project uses file from [HDX](https://data.humdata.org/dataset/geoboundaries-admin-boundaries-for-cambodia?).
+The project uses Shapefiles from [HDX](https://data.humdata.org/dataset/geoboundaries-admin-boundaries-for-cambodia?) Website.
 
 Sub-national Administrative Boundaries contain four files:
 1. geoBoundaries-KHM-ADM0.geojson
@@ -8,6 +8,6 @@ Sub-national Administrative Boundaries contain four files:
 3. geoBoundaries-KHM-ADM2.geojson
 4. geoBoundaries-KHM-ADM3.geojson
 
-These data files located in sf_data folder. 
+- These data files located in [sf_data](https://github.com/SopheaEVC/Rong-ARG/tree/main/sf_data) folder. 
 
 
--- RongARG.R
+++ RongARG.R
@@ -4,3 +4,42 @@ library(sf)
 library(tidyverse)
 
 # Loading files 
+crs = st_crs("+proj=longlat +datum=WGS84 +no_defs")
+n <- st_read("C:/Rstudio/Rong_ARG/Rong-ARG/sf_data/geoBoundaries-KHM-ADM0.geojson") %>%
+     st_as_sf() %>% 
+     st_transform(crs = crs)
+p <- st_read("C:/Rstudio/Rong_ARG/Rong-ARG/sf_data/geoBoundaries-KHM-ADM1.geojson") %>%
+     st_as_sf() %>% 
+     st_transform(crs = crs)
+ps.sf <- p %>%
+     filter(shapeName == "Preah Sihanouk")
+d <- st_read("C:/Rstudio/Rong_ARG/Rong-ARG/sf_data/geoBoundaries-KHM-ADM2.geojson") %>%
+     st_as_sf() %>% 
+     st_transform(crs = crs) 
+     
+c <- st_read("C:/Rstudio/Rong_ARG/Rong-ARG/sf_data/geoBoundaries-KHM-ADM3.geojson") %>%
+     st_as_sf() %>% 
+     st_transform(crs = crs)
+
+line.color <- "black"
+study_col <- "grey"
+ps.district 
+
+
+# Create Insect Map
+get_cam_map <- function(){
+     cam_map <- ggplot() +
+          geom_sf(data = n,
+                  fill = 'lightgrey', 
+                  color = line.color) +
+          geom_sf(data = p, 
+                  fill = 'lightgrey') +
+          geom_sf(data = ps.sf, 
+                  fill = study_col, 
+                  col = line.color)+
+          theme_void()
+     return(cam_map)
+}
+get_cam_map()
+
+
